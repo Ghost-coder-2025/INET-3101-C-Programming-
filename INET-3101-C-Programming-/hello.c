91ea041460d4e4b9053b493fe5fe9f5a6ba599a7
Mohamed <roble@gustavus.edu>
2026-09-13 14:27:22 -0500

    initial commit

---
+++ INET-3101-C-Programming-/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+My first program in C
+*/
+
+int main() {
+    printf("Hello INET 3101\n");
+    printf("In this class Im learning C programming");
+    return 0;
+}
+
+
+/*
+Day 1
+Learn how C is applied in today's world
+Thi*/
---
+++ myname.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/*
+My first program in C
+*/
+
+int main() {
+    printf("Hello INET 3101, My name is Mohamed Roble\n");
+    return 0;
+}
